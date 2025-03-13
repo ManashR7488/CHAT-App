300003ec60550ca755000dee6ea1047b24e49b5d
ManashR <mahanandmanash91@gmail.com>
2025-03-13 20:52:43 +0530

    alittle change

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,5 @@
 import axios from "axios"
 
-// const baseUrl = 'https://mr2-chats-backend.vercel.app/api' //backend url
-
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
     withCredentials: true,
